44d1bdc51c92c9f105d1c930af4602270f196e47
Dominik Honnef <dominik@honnef.co>
2023-04-07 14:52:30 +0200

    Unroll loops in count-min sketch
    While the unrolling prevents inlining (due to the syntactic complexity),
    it still results in a slight overall speedup.
    
    goos: linux
    goarch: amd64
    pkg: github.com/dgryski/go-tinylfu
    cpu: AMD Ryzen 9 3950X 16-Core Processor
                      │  old.bench  │              new.bench              │
                      │   sec/op    │   sec/op     vs base                │
    CMAddSaturated-32   5.182n ± 1%   4.562n ± 1%  -11.95% (n=50)
    CMEstimate-32       5.478n ± 1%   4.566n ± 1%  -16.64% (n=50)
    CMReset-32          4.031µ ± 1%   3.964µ ± 1%   -1.66% (p=0.000 n=50)
    Get-32              21.73n ± 0%   20.31n ± 1%   -6.53% (n=50)
    geomean             39.71n        35.99n        -9.37%

--- cm4.go
+++ cm4.go
@@ -26,29 +26,39 @@ func newCM4(w int) *cm4 {
 }
 
 func (c *cm4) add(keyh uint64) {
-	h1, h2 := uint32(keyh), uint32(keyh>>32)
+	// The loop unrolling prevents this function from being inlined, but it still results in a slight overall speedup.
+	c.s[3].inc(c.counterOffset(keyh, 3))
+	c.s[2].inc(c.counterOffset(keyh, 2))
+	c.s[1].inc(c.counterOffset(keyh, 1))
+	c.s[0].inc(c.counterOffset(keyh, 0))
+}
 
-	for i := range c.s {
-		pos := (h1 + uint32(i)*h2) & c.mask
-		c.s[i].inc(pos)
-	}
+func (c *cm4) counterOffset(keyh uint64, level int) uint32 {
+	// counterOffset gets inlined and the compiler removes the duplicated computations of h1 and h2, so there is no
+	// benefit to accepting h1 and h2 as arguments.
+	h1, h2 := uint32(keyh), uint32(keyh>>32)
+	return (h1 + uint32(level)*h2) & c.mask
 }
 
 func (c *cm4) estimate(keyh uint64) byte {
-	h1, h2 := uint32(keyh), uint32(keyh>>32)
-
 	var min byte = 255
-	for i := 0; i < depth; i++ {
-		pos := (h1 + uint32(i)*h2) & c.mask
-		v := c.s[i].get(pos)
+	bmin := func(v, min byte) byte {
+		// bmin gets inlined and the else branch gets optimized away
 		if v < min {
-			min = v
+			return v
+		} else {
+			return min
 		}
 	}
+	min = bmin(c.s[3].get(c.counterOffset(keyh, 3)), min)
+	min = bmin(c.s[2].get(c.counterOffset(keyh, 2)), min)
+	min = bmin(c.s[1].get(c.counterOffset(keyh, 1)), min)
+	min = bmin(c.s[0].get(c.counterOffset(keyh, 0)), min)
 	return min
 }
 
 func (c *cm4) reset() {
+	// There is no point in unrolling this loop, the cost is dominated by nvec.reset, which is O(n)
 	for _, n := range c.s {
 		n.reset()
 	}
